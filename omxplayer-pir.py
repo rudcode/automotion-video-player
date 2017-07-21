0d9b98155c78e2f63357605b3d6a578ef2b9a0a9
rudcode <rudynurhadi@gmail.com>
2017-07-21 10:20:10 +0000

    berubah

--- omxplayer-pir.py
+++ omxplayer-pir.py
@@ -23,6 +23,12 @@ import subprocess
 import logging
 import threading
 import time
+import thread
+import termios
+import tty
+from random import randint
+from omxplayer.player import OMXPlayer
+from omxplayer.player import logger
 import RPi.GPIO as GPIO
 
 __author__ = 'Jozef Hutting'
@@ -31,227 +37,106 @@ __license__ = 'GPLv2'
 __version__ = '0.14'
 
 # USAGE
-#     sudo python omxplayer-pir FILE
+#     sudo python omxplayer-pir FILE DURATION
 # where
 #     FILE is the name of the file to play 
 
 
-# the REAL OMXPlayer
-OMXPLAYER = 'omxplayer'
 # REAL OMXPLAYER options
 OMXPLAYER_ARGS = [
      #'--display=4',  # Raspberry Pi touchscreen
      '--no-osd', # Do not display status information on screen
-     '--loop' # Loop file.
+     #'--loop', # Loop file.
+     #'-b',
+     '--aspect-mode', #full screen
+     'stretch',
+     #'--aidx',
+     #'-1',
+     '--hw'
     ]
 
+#omxplayer wrapper variable
 omxplayer = None
-
-
-class OMXPlayer:
-
-    '''
-    Note: this python wrapper to control OMXPlayer is a very simple implementation.
-    If you want a more (on the edge) control over OMXPlayer, I suggest to use 
-    Will Price OMXPlayer wrapper (https://github.com/willprice/python-omxplayer-wrapper).
-    '''
-
-    process = None
-    running = False
-    completed = False
-
-    def __init__(self):
-        self.logger = logging.getLogger('__OMXPlayer__')
-
-    def log(self, args):
-        self.logger.debug('{0}'.format(args))
-
-    def log_error(self, args):
-        self.logger.error('{0}'.format(args))
-
-    def play(self, filename):
-
-        def run_in_thread():
-
-            #def monitor(process):
-            #    process.wait()
-
-            command = [OMXPLAYER]
-            command.extend(OMXPLAYER_ARGS)  # default arguments
-            command.append(filename)
-            self.log('Full command={0}'.format(command))
-            with open(os.devnull, 'w') as devnull:
-                self.process = subprocess.Popen(command,
-                                                stdin=subprocess.PIPE,
-                                                stdout=devnull,
-                                                stderr=devnull)
-
-            # wait for REAL OMXPlayer process is running
-            while self.process.poll() is not None:
-                time.sleep(0.01) # seconds
-                self.log('#')
-            self.log('process PID={0}'.format(self.process.pid))
-            self.running = True
-
-            while True:
-                if self.process.poll() is not None:
-                    break
-            self.log('REAL OMXPlayer exit status/return code : {0}'
-                     .format(self.process.returncode))
-            self.completed = True
-            return
-
-        if not os.path.isfile(filename):
-             self.log_error('Error: File "{0}" not found!'.format(filename))
-             raise IOError(filename)
-
-        self.thread = threading.Thread(target=run_in_thread, args=())
-        self.thread.start()
-        self.log('Waiting for running REAL OMXPlayer...') 
-        while(not self.running):
-            continue;
-        self.log('REAL OMXPlayer is up and running!') 
-
-    def quit(self):
-        p = self.process
-        if (p is not None):
-            try:
-                self.log('Quitting REAL OMXPlayer...')
-                self.running = False
-                self.__key(b'q')  # send quit command           
-                # wait for process termination 
-                self.thread.join() 
-            except:
-                self.log_error('Error upon quitting REAL OMXPlayer: {0}'
-                               .format(sys.exc_info()[0]))
-
-    def pause(self):
-        if self.running:
-            self.log('Pause REAL OMXPlayer')
-            self.__key(b' ') # SPACE character to pause
-
-    def resume(self):
-        if self.running:
-            self.log('Resume REAL OMXPlayer')
-            self.__key(b' ') # SPACE character to unpause
-
-    def __key(self, value):
-        if self.running:
-            try:
-                self.process.stdin.write(value)
-                self.log("Key b'{0}' sent successfully".format(value))
-            except:
-                self.log_error('Error on sending key to REAL OMXPlayer: {0}'
-                               .format(sys.exc_info()[0]))
-
 
 class PirControl():
 
-    '''
-    PIR (motion) sensor input signal :
-    - value 0 : no motion detected
-    - value 1 : motion detected
-    '''
-
     def __init__(self):
-        self.logger = logging.getLogger('__PirControl__')
-        # ------ H A R D W A R E configuration --------------------------------- 
-        #self.gpio = 22  # BCM port number!
-        #GPIO.setup(self.gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.gpio = 7  # BCM port number!
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        self.gpio = 14
         GPIO.setup(self.gpio, GPIO.IN)
-        # ----------------------------------------------------------------------
         self.state = self.__get_state()
-        self.log('initial state={0}'.format(self.state))
-
+        
     def __get_state(self):
         return GPIO.input(self.gpio)
 
     def start(self):
-        self.log('start')
-        GPIO.add_event_detect(self.gpio, GPIO.BOTH, callback=self.edge_callback
-                              )#, bouncetime=1)
+        GPIO.add_event_detect(self.gpio, GPIO.BOTH, callback=self.edge_callback)
         self.state = self.__get_state()
-        self.log('initial state={0}'.format(self.state))
-
-    def log(self, args):
-        self.logger.debug('{0}'.format(args))
 
     def edge_callback(self, channel):
-        global omplayer
+        global omxplayer
         state = self.__get_state()
-        self.log('Edge detected {0}=>{1}'.format(self.state, state))
-        # Hmmm...sometimes I get 0=0 and 1=>1 edges!?
-        # I ONLY want the real edges!
+
         if state != self.state:
-            if state == 1: # edge 0=>1
-                self.log('Motion detected!')
-                omxplayer.resume()
-            else: # edge 1=>0
-                self.log('NO motion detected')
-                omxplayer.pause();
+            if state == 1:
+                print 'Motion detected!'
+                omxplayer.play()
+            else:
+                print 'NO motion detected!'
             self.state = state
 
     def motion_detected(self):
         self.state = self.__get_state()
         return self.state == 1
 
-
 class Main():
-
-    def __init__(self):
-        self.logger = logging.getLogger('__Main__')
-
-    def log(self, args):
-        self.logger.debug('{0}'.format(args))
-
-    def log_error(self, args):
-        self.logger.error('{0}'.format(args))
 
     def run(self, filename):
       global omxplayer
 
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setwarnings(False)
-
-      omxplayer = OMXPlayer()
       pir_control = PirControl()
       return_code = 0 # OK
 
       try:    
 
-          self.log('Waiting for operator clearing the scene...');
           while(pir_control.motion_detected()):
               continue;
-          self.log('Video player is armed!');
-
-          # handle the PIR sensor signal edges
-          pir_control.start()
-
-          # OMXPlayer doesn't have the ability to start in a PAUSED state, and
-          # a command to start the PLAYING.
-          # Therefor start with a PLAYING OMXPlayer...
-          self.log("Start REAL OMXPlayer");
-          omxplayer.play(filename)
-          # ...but as soon it is playing, PAUSEd it, as we want it to start
-          # playing on motion detection.
-          self.log("Pausing REAL OMXPlayer");
+          
+          omxplayer = OMXPlayer(filename, OMXPLAYER_ARGS, None, None, True)
           omxplayer.pause()
 
-          # Wait here until OMXPlayer is done playing the file
-          # Note: when the default arguments OMXPLAYER_ARGS contains '--loop'
-          # the looping is stopped (aborted) by CTRL+C (KeyboardInterrupt event).
-          self.log("Waiting for REAL OMXPlayer played the file...");
-          while(not omxplayer.completed):
-               continue            
+          print 'Video player is armed!'
+          print str(omxplayer.duration())
+
+          pir_control.start()
+          
+          i = duration
+          
+          while True:
+              status = 0
+              
+              while status != 1:
+                  pos = omxplayer.position()
+                  #print str(pos)
+                  if pos >= i:    
+                      status = 1
+                  continue
+              if pir_control.motion_detected() == 0:
+                  omxplayer.pause()
+
+              i += duration
+              if i >= omxplayer.duration() - duration:
+                  i = duration
+                  omxplayer.set_position(0)
+                  
+              #print "i="+str(i)
 
       except IOError as e:
-          self.log_error('Error: File "{0}" not found!'.format(e.message))
           return_code = -1
 
       except KeyboardInterrupt:
-          """http://stackoverflow.com/questions/19807134/python-sub-process-ctrlc"""
-          self.log('KeyboardInterrupt')
+          print 'KeyboardInterrupt'
           omxplayer.quit()
 
       print('EXIT')
@@ -261,31 +146,33 @@ class Main():
 
 if __name__ == '__main__':
 
-    logging.basicConfig(level=logging.DEBUG)
+    logger.propagate = False
 
     # we need at least one FILE argument...
-    if len(sys.argv) < 2:
-        print('Error: missing FILE argument!')
+    if len(sys.argv) < 3:
+        print('Error: missing FILE and duration argument!')
         sys.exit(-3)
 
     # ... and at most one FILE argument
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         print('Error: too many arguments! Only one FILE argument expected.')
         sys.exit(-2)
 
     # so we only have one argument and that's the FILE argument
     filename = sys.argv[1]
+    duration = int(sys.argv[2])
 
     # check if the file exists
     if not os.path.isfile(filename):
         print('Error: File "{0}" not found!'.format(filename))
         sys.exit(-1)
-
+  
     # so far so good, so let's play :-)
     try:
         mainloop = Main();
         return_code = mainloop.run(filename);
+    
     finally:
         pass
- 
+       
     sys.exit(return_code);
